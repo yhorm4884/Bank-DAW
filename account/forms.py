eae3ecd32b1e407e7cf9e45c7e0f995755b243e5
Miguel Carballo <miguelcarballoglez@gmail.com>
2023-11-02 11:59:36 +0000

    Estilos cssy bootstrap login,registro

--- account/forms.py
+++ account/forms.py
@@ -4,9 +4,15 @@ from django.core.validators import FileExtensionValidator
 from .models import Profile
 
 
+
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+
 # Para que no exista problemas entre los modelos he creado un formulario que va vinculado con el del usuario
 # Este permite si queremos restringir
 class ProfileForm(forms.ModelForm):
@@ -15,14 +21,21 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['avatar']
+        
+
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
         fields = ['username', 'first_name', 'email']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'})
+        }
 
     def clean_password2(self):
         cd = self.cleaned_data
@@ -35,7 +48,6 @@ class UserRegistrationForm(forms.ModelForm):
         if User.objects.filter(email=data).exists():
             raise forms.ValidationError('Email already in use.')
         return data
-
 
 class UserEditForm(forms.ModelForm):
     class Meta:
@@ -54,3 +66,4 @@ class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['avatar']
+        
