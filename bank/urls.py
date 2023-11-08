93cbd5da7b0d7e06b7aed4a8c0c3d9a954541902
badel <badbonsim@gmail.com>
2023-11-08 00:29:11 +0000

    payments plantilla

--- bank/urls.py
+++ bank/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('client/', include('clients.urls')),
     path('', include('account.urls')),
+    path('', include('payments.urls')),
     path('cards/', include('cards.urls')),
 ]
 if settings.DEBUG:
