b7c52368d089e2c89d0cb999d58c42de44c1a868
Badel <badbonsim@gmail.com>
2024-02-02 16:11:15 +0000

    añadir comentarios

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from clients.models import Account, CreditCard
 from transactions.models import Transaction
+# Recordar que los serializadores sirven para indicar el modelo y los campos a mostrar dentro del json de la api
 
+# Tambien recordar importar en el settings.py rest_framework.renderers.JSONRenderer en el rest_framework para la visualizacion en json
+# mirad aqui https://www.django-rest-framework.org/api-guide/renderers/
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
